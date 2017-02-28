d56edc4388d2557906afa3f9c160994f3d7a501f
huangrob <robert.huang2000@gmail.com>
2017-02-28 09:17:32 -0800

    1

---
+++ TextExcel/src/textExcel/RealCell.java
@@ -0,0 +1,10 @@
+package textExcel;
+
+public class RealCell implements Cell{
+	public String abbreviatedCellText(){
+		return abbreviatedCellText();
+	}
+	public String fullCellText(){
+		return fullCellText();
+	}
+}
---
+++ TextExcel/src/textExcel/TextCell.java
@@ -0,0 +1,11 @@
+package textExcel;
+
+public class TextCell implements Cell{
+	public String abbreviatedCellText(){
+		return abbreviatedCellText();
+	}
+	public String fullCellText(){
+		return fullCellText();
+	}
+
+}
