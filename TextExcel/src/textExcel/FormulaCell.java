b624dfc82eee54253d055c3a803d07c1fdbce334
huangrob <robert.huang2000@gmail.com>
2017-04-13 08:35:02 -0700

    30

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -30,45 +30,62 @@ public class FormulaCell extends RealCell {
 		String[] operators = new String[operands.length];
 		String[] terms = new String[operands.length];
 		Double[] termsValue = new Double[operands.length];
-		for (int i = 0; i < operands.length; i++){
-			if (i % 2 == 1){
-				operators[i] = operands[i];
-			}
-			else {
-				terms[i] = operands[i];
-			}
-		}
-		for (int i = 0; i < terms.length; i+=2){
-			if ((terms[i].charAt(0) >= 'a' && terms[i].charAt(0) <= 'z') || (terms[i].charAt(0) >= 'A' && terms[i].charAt(0) <= 'Z')){
-				SpreadsheetLocation loc = new SpreadsheetLocation(terms[i].toUpperCase());
-				String cellFormula = spreadsheet[loc.getCol()][loc.getRow()].fullCellText();
-				FormulaCell cell = new FormulaCell(cellFormula, spreadsheet);
-				termsValue[i] = cell.getDoubleValue();
-			}
-			else {
-				termsValue[i] = Double.parseDouble(terms[i]);
-			}
-		}
-		Double returnValue = termsValue[0];
-		if (operands.length == 1){
-			return returnValue;
-		}
-		else {
-			for (int i = 0; i < termsValue.length - 2; i+=2){
-				if (operators[i+1].equals("+")){
-					returnValue += termsValue[i+2];
+		if (!operands[0].equals("AVG") && !operands[0].equals("SUM")) {
+			for (int i = 0; i < operands.length; i++){
+				if (i % 2 == 1){
+					operators[i] = operands[i];
 				}
-				else if (operators[i+1].equals("-")){
-					returnValue -= termsValue[i+2];
-				}	
-				else if (operators[i+1].equals("*")){
-					returnValue *= termsValue[i+2];
-				}	
-				else if (operators[i+1].equals("/")){
-					returnValue /= termsValue[i+2];
+				else {
+					terms[i] = operands[i];
 				}
 			}
-			return returnValue;
+			for (int i = 0; i < terms.length; i+=2){
+				if ((terms[i].charAt(0) >= 'a' && terms[i].charAt(0) <= 'z') || (terms[i].charAt(0) >= 'A' && terms[i].charAt(0) <= 'Z')){
+					SpreadsheetLocation loc = new SpreadsheetLocation(terms[i].toUpperCase());
+					String cellFormula = spreadsheet[loc.getCol()][loc.getRow()].fullCellText();
+					FormulaCell cell = new FormulaCell(cellFormula, spreadsheet);
+					termsValue[i] = cell.getDoubleValue();
+				}
+				else {
+					termsValue[i] = Double.parseDouble(terms[i]);
+				}
+			}
+			Double returnValue = termsValue[0];
+			if (operands.length == 1){
+				return returnValue;
+			}
+			else {
+				for (int i = 0; i < termsValue.length - 2; i+=2){
+					if (operators[i+1].equals("+")){
+						returnValue += termsValue[i+2];
+					}
+					else if (operators[i+1].equals("-")){
+						returnValue -= termsValue[i+2];
+					}	
+					else if (operators[i+1].equals("*")){
+						returnValue *= termsValue[i+2];
+					}	
+					else if (operators[i+1].equals("/")){
+						returnValue /= termsValue[i+2];
+					}
+				}
+				return returnValue;
+			}
+		}
+		else if (operands.length == 2) {
+			String[] cellRange = operands[0].split("-");
+			Double[]
+			for (char c = 'A'; c <= 'L'; c++){
+				for (int i = 0; i < 20; i++){
+					
+				}
+			}
+			if (operands[0].equals("AVG")){
+				
+			}
+			else if (operands[0].equals("SUM")){
+				
+			}
 		}
 	}
 	
